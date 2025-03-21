ea53d9e838a5050bdc9a69cd53c3ff01e934bff8
Killian KOPP <killiankopp@gmail.com>
2025-03-21 13:23:03 +0100

    Update create-secret.sh

--- CD/mongodb/create-secret.sh
+++ CD/mongodb/create-secret.sh
@@ -1,3 +1,3 @@
 kubectl create secret generic mongodb-server-license-secret -n karned \
   --from-literal=mongodb-root-password="supermotdepasse" \
-  --from-literal=mongodb-password="autresupermotdepasse"
+  --from-literal=mongodb-passwords="autresupermotdepasse"
